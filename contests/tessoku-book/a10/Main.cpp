8f2d360666c054a651656048c944ec78a7ec1b91
Keisuke Yanagimachi <willowtown0576@gmail.com>
2026-01-30 04:09:57 +0900

    第2章累積和 完了

--- contests/tessoku-book/a10/Main.cpp
+++ contests/tessoku-book/a10/Main.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <atcoder/all>
 #include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 using namespace atcoder;
@@ -18,6 +20,8 @@ using ll = long long;
 using ull = unsigned long long;
 using i128 = __int128_t;
 using u128 = __uint128_t;
+using vi = vector<int>;
+using vll = vector<ll>;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 
@@ -27,7 +31,9 @@ constexpr ll LINF = numeric_limits<ll>::max() / 4;
 
 // ---- macros ----
 #define rep(i, x, limit) for (ll i = (ll)(x); i < (ll)(limit); i++)
+#define rrep(i, x, limit) for (ll i = (ll)(limit) - 1; i >= (ll)(x); i--)
 #define REP(i, x, limit) for (ll i = (ll)(x); i <= (ll)(limit); i++)
+#define RREP(i, x, limit) for (ll i = (ll)(limit); i >= (ll)(x); i--)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define el '\n'
@@ -40,10 +46,10 @@ constexpr ll LINF = numeric_limits<ll>::max() / 4;
 #endif
 
 // ---- helpers ----
-inline void Yes(){ cout << "Yes\n"; }
-inline void No(){ cout << "No\n"; }
-inline void YES(){ cout << "YES\n"; }
-inline void NO(){ cout << "NO\n"; }
+inline void Yes() { cout << "Yes\n"; }
+inline void No() { cout << "No\n"; }
+inline void YES() { cout << "YES\n"; }
+inline void NO() { cout << "NO\n"; }
 
 template <class T> inline bool chmax(T &a, const T &b) {
   if (a < b) {
@@ -81,7 +87,29 @@ template <class T> void print_vec(const vector<T> &v, char sep = ' ') {
 
 // ---- solve ----
 static void solve() {
-  // TODO: write your solution here
+  int n;
+  cin >> n;
+  vi a = read_vec<int>(n);
+
+  vi p(n + 1, 0);
+  p[1] = a[0];
+  REP(i, 2, n) { p[i] = max(p[i - 1], a[i - 1]); }
+
+  vi q(n + 1, 0);
+  q[n] = a[n - 1];
+  RREP(i, 1, n - 1) { q[i] = max(q[i + 1], a[i - 1]); };
+
+  // print_vec(p);
+  // print_vec(q);
+
+  int d;
+  cin >> d;
+
+  rep(i, 0, d) {
+    int l, r;
+    cin >> l >> r;
+    cout << max(p[l - 1], q[r + 1]) << el;
+  }
 }
 
 // ---- main ----
